f433931172e1e4a47b4d0f5a9aad5a60312e7e6e
IamBusy <1342247033@qq.com>
2017-07-28 16:18:21 +0800

    Add phpmyadmin container

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,1 +1,3 @@
 docker run --name mysql -e MYSQL_ROOT_PASSWORD=123456 -p "3306:3306" -d mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+docker run --name phpmyadmin -d --link mysql:db -p 8090:80 -e PMA_ARBITRARY=1 phpmyadmin/phpmyadmin
