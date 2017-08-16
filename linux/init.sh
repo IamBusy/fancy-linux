8b6ee86572925d286dc9ebfa0a9df6bfa82aebd4
IamBusy <1342247033@qq.com>
2017-08-16 10:33:35 +0800

    Update install-script

--- linux/init.sh
+++ linux/init.sh
@@ -1,6 +1,15 @@
-yum install zsh -y;
-yum install git -y;
+sudo yum install -y zsh git;
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";
-yum install autojump-zsh -y;
+sudo yum install autojump-zsh -y;
 sed -i "s/plugins=(git)/plugins=(git autojump)/g" ~/.zshrc
 chsh -s /bin/zsh
+
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl start docker
